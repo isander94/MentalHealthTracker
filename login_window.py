52ef54aa77285b6effadf04d71c18b5e3adc64c6
Leon Kardum <leon.kardum0040@stud.hkr.se>
2024-04-22 14:37:40 +0200

    Added password, can now get username and password, + small changes

--- login_window.py
+++ login_window.py
@@ -5,11 +5,18 @@ root.geometry("900x900")
 root.title("Log In")
 
 usernameLabel = Label(root, text="Username", fg ="blue", bg="lightblue")
-usernameLabel.place(x=450, y=350)
+usernameLabel.place(x=350, y=250)
 
-entry = Entry(root)
+userEntry = Entry(root)
+userEntry.place(x=350,y=270)
+username = userEntry.get()
 
-entry.place(x=450,y=370)
+passwordLabel = Label(root, text="Password", fg="blue", bg="lightblue")
+passwordLabel.place(x=350,y=300)
+
+passwordEntry = Entry(root)
+passwordEntry.place(x=350, y=320)
+password = passwordEntry.get()
 
 
 root.mainloop()
