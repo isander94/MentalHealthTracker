32929bf707dc0f1b94e371f2f0bc575bb321644d
mofras <maame-4@hotmail.com>
2024-04-23 14:29:32 +0200

    bagg fixed

--- sing_up_window.py
+++ sing_up_window.py
@@ -76,30 +76,44 @@ def sign_up_window():
     print("Password:", password)
 
 '''crating labels and entry fields for sign in'''
+first_name_label = Label(root, text="Email:")
+first_name_label.grid(padx=10, pady=5, sticky="e")
+first_nam_label.place(x=350, y=230)
+first_name_entry. = Entry(root)
+first_name_entry.grid(padx=10, pady=5)
+first_name_entry.place(x=350, y=250)
+
+last_name_label = Label(root, text="Email:")
+last_name_label.grid(padx=10, pady=5, sticky="e")
+last_name_label.place(x=350, y=270)
+last_name_entry = Entry(root)
+last_name_entry.grid(padx=10, pady=5)
+last_name_entry.place(x=350, y=300)
+
 email_label = Label(root, text="Email:")
 email_label.grid(padx=10, pady=5, sticky="e")
-email_label.place(x=350, y=230)
+email_label.place(x=350, y=320)
 email_entry = Entry(root)
 email_entry.grid(padx=10, pady=5)
-email_entry.place(x=350, y=250)
+email_entry.place(x=350, y=350)
 
 password_label = Label(root, text="Password:")
 password_label.grid(padx=10, pady=5, sticky="e")
-password_label.place(x=350, y=270)
+password_label.place(x=350, y=370)
 password_entry = Entry(root, show="*")
 password_entry.grid(padx=10, pady=5)
-password_entry.place(x=350, y=300)
+password_entry.place(x=350, y=400)
 
 
 '''Creating login buttons'''
 
 login_button = Button(root, text="Login", command=login)
 login_button.grid(padx=10, pady=10)
-login_button.place(x=350, y=320)
+login_button.place(x=350, y=420)
     
 create_account_button = Button(root, text="Create Account", command=sign_up_fields)
 create_account_button.grid(padx=10, pady=5)
-create_account_button.place(x=350, y=400)
+create_account_button.place(x=350, y=450)
 
 root.mainloop()
 
