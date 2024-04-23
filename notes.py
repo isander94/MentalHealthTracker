7a0bd26cb65d078c2b62c29b69e8bbd13b79b9d0
Leon Kardum <leon.kardum0040@stud.hkr.se>
2024-04-23 15:26:04 +0200

    Added few functions

--- notes.py
+++ notes.py
@@ -1,8 +1,24 @@
 
-def getDay():
-    pass
 
-def WriteNotes():
-    pass
+class Notes:
+    def __init__(self):
+        pass
 
+    def getDay():
+        pass
+
+    def WriteNotes():
+        pass
+
+    def saveNotes():
+        pass
+
+    def getNotes():
+        pass
+
+    def SetMood():
+        pass
+
+    def getMood():
+        pass
 
