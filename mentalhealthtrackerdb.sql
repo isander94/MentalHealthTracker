0cba755bfddffafa9335453ab5c1492975142068
Mohammed-Nur Nur <mohammed-nur.nur0002@stud.hkr.se>
2024-04-25 13:11:05 +0200

    Added password column to the database

--- mentalhealthtrackerdb.sql
+++ mentalhealthtrackerdb.sql
@@ -3,12 +3,13 @@ create table users (
 user_id int,
 first_name varchar(50) not null,
 last_name varchar(50) not null,
-email varchar(70) not null
+email varchar(70) not null,
+password varchar(20) not null
 );
 
 insert into users
-values	(1, "Alex", "Alexsson", "axel@gmail.com"),
-		(2, "Oscar", "Alexsson", "axel@gmail.com"),
-		(3, "Ala", "Alexsson", "axel@gmail.com");
+values	(1, "Alex", "Alexsson", "axel@gmail.com", "123"),
+		(2, "Oscar", "Alexsson", "axel@gmail.com", "123"),
+		(3, "Ala", "Alexsson", "axel@gmail.com", "123");
 
 select * from users;
