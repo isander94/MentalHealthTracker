e9734ddf556d65d07807ae288af94f2b70451a82
Mohammed-Nur Nur <mohammed-nur.nur0002@stud.hkr.se>
2024-04-25 13:04:26 +0200

    Added code that creates the database

--- mentalhealthtrackerdb.sql
+++ mentalhealthtrackerdb.sql
@@ -1,4 +1,10 @@
 use mentalHealthTrackerDB;
+create table users (
+user_id int,
+first_name varchar(50) not null,
+last_name varchar(50) not null,
+email varchar(70) not null
+);
 
 insert into users
 values	(1, "Alex", "Alexsson", "axel@gmail.com"),
