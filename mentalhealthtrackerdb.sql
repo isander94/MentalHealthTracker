734f2c9387367fae027713485ae600e941176355
Mohammed-Nur Nur <mohammed-nur.nur0002@stud.hkr.se>
2024-04-25 13:03:16 +0200

    Added the database

---
+++ mentalhealthtrackerdb.sql
@@ -0,0 +1,8 @@
+use mentalHealthTrackerDB;
+
+insert into users
+values	(1, "Alex", "Alexsson", "axel@gmail.com"),
+		(2, "Oscar", "Alexsson", "axel@gmail.com"),
+		(3, "Ala", "Alexsson", "axel@gmail.com");
+
+select * from users;
