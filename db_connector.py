373bb712d1faef2a3caf349c2929b8bba586c7f7
Mohammed-Nur Nur <mohammed-nur.nur0002@stud.hkr.se>
2024-04-26 18:22:29 +0200

    Added db_connector.py from F-branch

--- db_connector.py
+++ db_connector.py
@@ -1,50 +1,52 @@
 import mysql.connector
-
-'''Establishing a connection to the MySQL database'''
-db_connection = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="team16",
-    database="mentalHealthTrackerDB"
-)
-
-
-def login(self, email, password):
-    '''Function to handle user login'''
-    cursor = db_connection.cursor()
-
-    '''Check if email and password match a record in the database'''
-    cursor.execute("SELECT * FROM users WHERE email = %s AND password = %s ORDER BY email, password", (email, password))
-    users = cursor.fetchall()
-    for user in users:
-        print(user)
-
-    if user:
-        print("Login successful")
-        return True
-    else:
-        print("Invalid email or password")
-        return False
+class DatabaseManager():
+    def __init__(self):
+        '''Establishing a connection to the MySQL database'''
+        self.db_connection = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="Sahra123",
+            database="mentalHealthTrackerDB"
+        )
 
 
-def sign_up(self):
-    '''Function to handle user sign up'''
-    first_name = first_name_entry.get()
-    last_name = last_name_entry.get()
-    email = email_entry.get()
-    password = password_entry.get()
+    def user_credentials(self, email, password):
+        '''Function to handle user login'''
+        cursor = self.db_connection.cursor()
 
-    cursor = db_connection.cursor()
+        '''Check if email and password match a record in the database'''
+        cursor.execute("SELECT * FROM users WHERE email = %s AND password = %s ORDER BY email, password", (email, password))
+        user = cursor.fetchall()
+        #for user in users:
+            #print(user)
 
-    ''' Check if email already exists in the database'''
-    cursor.execute("SELECT * FROM users WHERE email = %s", (email))
-    existing_user = cursor.fetchone()
+        if user:
+            #print("Login successful")
+            return True
+        else:
+            #print("Invalid email or password")
+            return False
 
-    if existing_user:
-        print("Email already exists")
-    else:
-        '''Insert new user into the database'''
-        cursor.execute("INSERT INTO users VALUES (%s, %s, %s, %s)", (first_name, last_name, email, password))
 
-        db_connection.commit()
-        print("User created successfully")
+    def add_user(self):
+        '''Function to handle user sign up'''
+        first_name = self.first_name_entry.get()
+        last_name = self.last_name_entry.get()
+        email = self.email_entry.get()
+        password = self.password_entry.get()
+
+        cursor = self.db_connection.cursor()
+
+        ''' Check if email already exists in the database'''
+        cursor.execute("SELECT * FROM users WHERE email = %s", (email))
+        existing_user = cursor.fetchone()
+
+        if existing_user:
+            return False
+            #print("Email already exists")
+        else:
+            '''Insert new user into the database'''
+            cursor.execute("INSERT INTO users VALUES (%s, %s, %s, %s)", (first_name, last_name, email, password))
+
+            db_connection.commit()
+            #print("User created successfully")
