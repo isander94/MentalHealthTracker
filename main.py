bcf00fa0393de885529de17d16f78b3373d797aa
Mohammed-Nur Nur <mohammed-nur.nur0002@stud.hkr.se>
2024-04-26 18:40:19 +0200

    Added main from newBranch

--- main.py
+++ main.py
@@ -7,9 +7,19 @@ class Main(Toplevel):
 
         super().__init__(root)
         self.title("Menu")
-        self.geometry("200x200")
-        label = Label(self, text="Welcome to the menu")
+        self.geometry("900x900")
+        label = Label(self, text="Welcome to the menu\nWhat do you want to do today?")
         label.pack()
+        
+        notes = Button(self, text="Daily journal")
+        notes.pack()
 
-    def menu(root):
-        pass
+        stats = Button(self, text="This weeks statistics")
+        stats.pack()
+
+        quit = Button(self, text="quit program", command=self.on_close)
+        quit.pack()
+
+    def on_close(self):
+        self.destroy()
+        self.master.destroy()
