dc7013cec1d14e48c0d93503d1341bc5d93613e5
Mohammed-Nur Nur <mohammed-nur.nur0002@stud.hkr.se>
2024-04-28 20:37:37 +0200

    Changed code in database

--- mentalhealthtrackerdb.sql
+++ mentalhealthtrackerdb.sql
@@ -7,9 +7,9 @@ email varchar(70) not null,
 password varchar(20) not null
 );
 
-insert into users
-values	(1, "Alex", "Alexsson", "axel@gmail.com", "123"),
-		(2, "Oscar", "Alexsson", "axel@gmail.com", "123"),
-		(3, "Ala", "Alexsson", "axel@gmail.com", "123");
+insert into users (first_name, last_name, email, password)
+values	("Alex", "Alexsson", "axel@gmail.com", "123"),
+		("Oscar", "Alexsson", "axel@gmail.com", "123"),
+		("Ala", "Alexsson", "axel@gmail.com", "123");
 
 select * from users;
