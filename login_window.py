ea7987ca19b0430ab76fe96326e6b58f4a8c042d
Mohammed-Nur Nur <mohammed-nur.nur0002@stud.hkr.se>
2024-04-30 20:05:43 +0200

    Added code from newBranch

--- login_window.py
+++ login_window.py
@@ -1,76 +1,54 @@
 from tkinter import *
 from tkinter.ttk import *
 import main
-from sign_up_window import LoginSystem
+import sign_up_window
 from db_connector import DatabaseManager
 
-class LoginWindow():
-    def __init__(self, root, user_db):
-        self.email_label = None
-        self.email_entry = None
-        self.password_label = None
-        self.password_entry = None
-        self.message_label = None
-        self.root = root
-        self.user_db = user_db
-        root.title("Log in")
-        root.geometry("900x900")
+class LoginWindow(Tk):
+
+    def __init__(self):
+        super().__init__()
+        self.title("Log in")
+        self.geometry("900x900")
 
         self.window()
 
-        self.root.mainloop()
+        self.mainloop()
 
     def window(self):
-        emailLabel = Label(self.root, text="Email")
-        emailLabel.place(x=350, y=250)
+        usernameLabel = Label(self, text="Username")
+        usernameLabel.place(x=350, y=250)
 
-        emailEntry = Entry(self.root)
-        emailEntry.place(x=350,y=270)
-        email = emailEntry.get()
+        userEntry = Entry(self)
+        userEntry.place(x=350,y=270)
+        username = userEntry.get()
 
-        passwordLabel = Label(self.root, text="Password")
+        passwordLabel = Label(self, text="Password")
         passwordLabel.place(x=350,y=300)
 
-        passwordEntry = Entry(self.root)
+        passwordEntry = Entry(self)
         passwordEntry.place(x=350, y=320)
         password = passwordEntry.get()
-        #user_db.user_credentials(email, password)
-         
+
         #login = main.Main.menu(root)
         #log = main
-        LoginButton = Button(self.root, text="Login", command=lambda: self.check_login_details(email, password))
+        LoginButton = Button(self, text="Login", command=self.openMenu)
         LoginButton.place(x=350, y=340)
-        
        # LoginButton.bind("<Button>", lambda e:log.Main(self))
 
         #signUp = sign_up_window
-        createAccountButton = Button(self.root, text="Create account", command=self.openSignUp)
+        createAccountButton = Button(self, text="Create account", command=self.openSignUp)
         createAccountButton.place(x=350,y=400)
         #createAccountButton.bind("<Button>", lambda e: signUp.LoginSystem(self) )
 
     def openMenu(self):
         main.Main(self)
-        root.withdraw()
-    
-    def check_login_details(self, email, password):
-        if self.user_db.user_credentials(email, password):
-            self.openMenu()
-            root.withdraw()
-        else:
-            pass
-            
-    def openSignUp(self):
-        print("In openSignUp")
-        login = LoginSystem(self.root, DatabaseManager())
-        login.sign_up_fields()
-        #self.withdraw()
-        
-        print("Closing openSign")
+        self.withdraw()
 
-# Initializing the tkinter module 
-root = Tk()
-# user_db gets access to all methods that connect to database
-# see db_connector.py for functions
-user_db = DatabaseManager()
-# Calling the constructor of the class (Running the program)
-LoginWindow(root, user_db)
+    def openSignUp(self):
+        user_db = DatabaseManager()
+        sign_up_window.LoginSystem(self, user_db)
+        self.withdraw()
+
+
+LoginWindow()
