50bfcfcde08b7158bfad31151a92d42ed8705551
Noorixyz <160539288+Noorixyz@users.noreply.github.com>
2024-05-12 18:26:40 +0200

    Update db_connector.py

--- db_connector.py
+++ db_connector.py
@@ -1,5 +1,12 @@
 import mysql.connector
+from datetime import datetime
 class DatabaseManager():
+    first_name = None
+    last_name = None
+    email = None
+    password = None
+    user_id = None
+        
     def __init__(self):
         '''Establishing a connection to the MySQL database'''
         self.db_connection = mysql.connector.connect(
@@ -8,21 +15,17 @@ class DatabaseManager():
             password="team16",
             database="mentalhealthtrackerdb"
         )
-        self.first_name = None
-        self.last_name = None
-        self.email = None
-        self.password = None
         
 
 
     def user_credentials(self, email, password):
         '''Function to handle user login'''
         cursor = self.db_connection.cursor()
-        self.email = email
+        DatabaseManager.email = email
         self.password = password
         '''Check if email and password match a record in the database'''
         sql_query = "SELECT * FROM users WHERE email = %s AND password = %s;"
-        data = (self.email, self.password,)
+        data = (email, password,)
         cursor.execute(sql_query, data)
         users = cursor.fetchall()
         print(users)
@@ -39,7 +42,6 @@ class DatabaseManager():
 
     def add_user(self, first_name, last_name, email, password):
         '''Function to handle user sign up'''
-        print("Here")
         cursor = self.db_connection.cursor()
 
         self.first_name = first_name 
@@ -49,7 +51,7 @@ class DatabaseManager():
 
         
         ''' Check if email already exists in the database'''
-        cursor.execute("SELECT * FROM users WHERE email = %s;", (email,))
+        cursor.execute("SELECT * FROM users WHERE email = %s;", (self.email,))
         existing_user = cursor.fetchall()
 
         if existing_user:
@@ -76,3 +78,40 @@ class DatabaseManager():
         self.db_connection.commit()
         cursor.close()
             
+    def add_note(self, note):
+        cursor = self.db_connection.cursor()
+        
+        query1 = "SELECT user_id FROM users WHERE email = %s;"
+        data1 = (self.email,)
+        print(DatabaseManager.email)
+        cursor.execute(query1, data1)
+        user_id = cursor.fetchone()
+        if user_id:    
+            query2 = "INSERT INTO notes (note, user_id, date_now) VALUES (%s, %s, %s);"
+            data2 = (note, user_id[0], datetime.now())
+            cursor.execute(query2, data2)
+            self.db_connection.commit()
+            cursor.close()
+            return True
+        else:
+            print("User not found")
+            cursor.close()
+            return False
+    
+    def previous_notes(self):
+        cursor = self.db_connection.cursor()
+        query1 = "SELECT user_id FROM users WHERE email = %s;"
+        data1 = (self.email,)
+        cursor.execute(query1, data1)
+        user_id = cursor.fetchone()
+        if user_id:        
+            query = "SELECT note from notes where user_id = %s"
+            data = (user_id[0],)
+            
+            cursor.execute(query, data)
+            print(cursor.fetchall())
+            previous_notes = cursor.fetchall()
+            self.db_connection.commit()
+            return previous_notes
+        else:
+            print("Error")
