adce7f3f90e5d6ec26ed9df1225db622fa1bd114
Mohammed-Nur Nur <mohammed-nur.nur0002@stud.hkr.se>
2024-05-12 18:22:58 +0200

    Creatd new database and added functionality to be able to add notes to the database

--- daily_journal.py
+++ daily_journal.py
@@ -1,6 +1,6 @@
 from tkinter import *
 from tkinter.ttk import *
-
+from db_connector import DatabaseManager
 def rgb_to_hex(rgb):
     """Convert RGB color to HEX format"""
     return "#{:02x}{:02x}{:02x}".format(*rgb)
@@ -11,11 +11,15 @@ class DailyJournal(Toplevel):
         super().__init__(parent)
         self.title("Daily Journal")
         self.geometry("900x900")
-        
         # Background color
         bg_color = rgb_to_hex((120, 163, 156))
         self.configure(background=bg_color)
-
+        self.user_db = DatabaseManager()
+        # For message when pressing save
+        self.message_label = None
+        self.message_label = Label(self, text="", foreground="red")
+        self.message_label.pack()
+        
         # Frame and alignment for "Mood buttons"
         mood_frame = Frame(self)
         mood_frame.pack(pady=10)
@@ -37,7 +41,8 @@ class DailyJournal(Toplevel):
         #Button to save the journal
         save_button = Button(self, text="Save", command=self.save) #,command=self.save)
         save_button.pack(pady=10)
-        
+        previous_notes_button = Button(self, text="Previous notes", command=self.previous_notes) #,command=self.save)
+        previous_notes_button.pack(pady=20)
         #Quit button to exit the application
         quit_button = Button(self, text="quit", command=self.on_close)
         quit_button.pack(pady=5)
@@ -48,15 +53,19 @@ class DailyJournal(Toplevel):
     def save(self):
         """function to save journal"""
         text = self.text_area.get("1.0", "end-1c")
-        with open("note.txt", "w") as file:
-            file.write(str(text))
-
-
+        if self.user_db.add_note(text):
+            self.message_label.config(text="Note saved successfully")
+        else:
+            self.message_label.config(text="An error occured")
+            
     def on_close(self):
         """Close the application."""
         self.destroy()
         self.master.destroy()
     
+    def previous_notes(self):
+        previous_notes = self.user_db.previous_notes()
+        
     def go_back(self):
         """Function is used to go back to the login window"""
         self.destroy() # closes the sign up window
