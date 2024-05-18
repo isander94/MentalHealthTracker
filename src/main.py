f6ff059a8e274155e1b577fdfa3f341b565086aa
Isaac Andersson <isaac.andersson0015@stud.hkr.se>
2024-05-18 14:31:43 +0200

    Added functionality for graph

--- src/main.py
+++ src/main.py
@@ -4,6 +4,8 @@ from daily_journal import DailyJournal
 from previous_notes import Previous_notes
 from statistic import Statistic
 from db_connector import DatabaseManager
+import matplotlib.pyplot as pyplot
+
 def rgb_to_hex(rgb):
     """Convert RGB color to HEX format"""
     return "#{:02x}{:02x}{:02x}".format(*rgb)
@@ -51,9 +53,37 @@ class Main(Toplevel):
         self.withdraw()
 
     def Statistic_window(self):
-        statistic = Statistic(self)
-        statistic.grab_set()
-        self.withdraw()
+        # statistic = Statistic(self)
+        # statistic.grab_set()
+        # self.withdraw()
+        
+            # Fetch notes and moods
+        moods = self.user_db.get_mood(self.user_db.email)
+        print("Contents 1:")
+        print(moods)
+        
+            # Create lists where dates and lists will be stored
+        dates_list = []
+        moods_list = []
+            # Extract the moods and dates from tuples and place in a list
+        for x in range(len(moods)):
+            dates_list.append(moods[x][3])
+            moods_list.append(moods[x][1])
+        print("Data added to lists")
+        print("Contents:")
+        print(dates_list)
+        print(moods_list)
+
+            # Take the last 7 dates and moods
+        dates_list = dates_list[-7:]
+        moods_list = dates_list[-7:]
+        print("Last 7 extracted")
+
+        pyplot.plot(dates_list, moods_list)
+        pyplot.show()
+
+
+
     
     def go_back(self):
         """Function is used to go back to the login window"""
