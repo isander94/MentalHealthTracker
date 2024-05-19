d9da33ac7c08c63e97518d7ad46e92699ceb7bee
mofras <maame-4@hotmail.com>
2024-05-19 14:28:29 +0200

    moved logout button

--- src/main.py
+++ src/main.py
@@ -10,7 +10,7 @@ from datetime import datetime
 def rgb_to_hex(rgb):
     """Convert RGB color to HEX format"""
     return "#{:02x}{:02x}{:02x}".format(*rgb)
-    
+
 class Main(Toplevel):
 
     def __init__(self, root = None):
@@ -26,7 +26,7 @@ class Main(Toplevel):
         self.configure(background=bg_color)
         label_font = ("Helvetica", 16, "bold")
         label = Label(self, text=f"Welcome {self.user_db.first_name}\nWhat do you want to do today?",
-             foreground="brown", font=label_font, background=bg_color, anchor="center")
+             foreground="white", font=label_font, background=bg_color, anchor="center")
         label.pack(pady=20)
 
         button_width = 20
@@ -35,26 +35,26 @@ class Main(Toplevel):
         stats = Button(self, text="This weeks statistics", width=button_width, command=self.Statistic_window)
         previous_notes = Button(self, text="View notes", width=button_width,  command=self.Previous_notes_window)
         quit_button = Button(self, text="Quit program", width=button_width, command=self.on_close)
-        log_out = Button(self, text="Log out", width=button_width, command=self.go_back)
+        log_out = Button(self, text="Log out", width=15, command=self.go_back)
 
         # Centering buttons vertically and horizontally
-        button_y_positions = [0.3, 0.4, 0.5, 0.6, 0.7]
+        button_y_positions = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7]
 
-        notes.place(relx=0.5, rely=button_y_positions[0], anchor=CENTER)
-        stats.place(relx=0.5, rely=button_y_positions[1], anchor=CENTER)
-        previous_notes.place(relx=0.5, rely=button_y_positions[2], anchor=CENTER)
-        quit_button.place(relx=0.5, rely=button_y_positions[3], anchor=CENTER)
-        log_out.place(relx=0.5, rely=button_y_positions[4], anchor=CENTER)
-    
+        notes.place(relx=0.5, rely=button_y_positions[2], anchor=CENTER)
+        stats.place(relx=0.5, rely=button_y_positions[3], anchor=CENTER)
+        previous_notes.place(relx=0.5, rely=button_y_positions[4], anchor=CENTER)
+        quit_button.place(relx=0.5, rely=button_y_positions[5], anchor=CENTER)
+        log_out.place(relx=0.9, rely=button_y_positions[0], anchor=CENTER)
+
     def on_close(self):
         self.destroy()
         self.master.destroy()
-    
+
     def Daily_journal_window(self):
         journal_window = DailyJournal(self)
         journal_window.grab_set()
         self.withdraw()
-    
+
     def Previous_notes_window(self):
         previous_notes = Previous_notes(self)
         previous_notes.grab_set()
@@ -76,11 +76,11 @@ class Main(Toplevel):
             for x in range (len(dates_list)):
                 date_string = dates_list[x].strftime("%m-%d")
                 dates_list[x] = date_string
-                
+
             # Take the last 7 dates and moods
             dates_list = dates_list[-7:]
             moods_list = moods_list[-7:]
-            
+
 
             pyplot.title("Mood statistics")
             pyplot.xlabel("Date")
